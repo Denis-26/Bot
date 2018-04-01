4b9e244bd781935edeb853dabd5b9e0fbc432118
Denis-26 <kartavenkodenis0@gmail.com>
2018-04-01 17:11:40 +0300

    Update __init__.py

--- Bot/telegram/__init__.py
+++ Bot/telegram/__init__.py
@@ -8,5 +8,5 @@ from .tools.InlineMode import (
 )
 
 from .tools.Payments import LabeledPrice
-from .tools.ReplyKeyboardMarkup import ReplyKeyboardMarkup, make_reply_buttons
+from .tools.ReplyKeyboardMarkup import ReplyKeyboardMarkup, make_reply_buttons, keyboard_remove
 from .telegram import Bot
